f87b756d1ce7a3d4e8a4e1d6ff6f63544426435a
Chathura Dilshan Guruge <152064583+csdgurugegit@users.noreply.github.com>
2024-05-19 16:39:43 +0530

    Update fonixweb.tf

--- terraform/fonixweb.tf
+++ terraform/fonixweb.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
- subscription_id  = ""
- client_id	      = ""
- client_secret    = ""
- tenant_id        = ""
+ subscription_id = ""
+ client_id	     = ""
+ client_secret   = ""
+ tenant_id       = ""
  features {}
 }
 
